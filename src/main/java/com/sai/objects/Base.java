d9cefb0002e25696301ed981980e4ebed0c33acc
cb-prasanna <sairam@chargebee.com>
2020-11-15 02:56:44 +0530

    Format Change Test

--- src/main/java/com/sai/objects/Base.java
+++ src/main/java/com/sai/objects/Base.java
@@ -3,4 +3,8 @@ package com.sai.objects;
 /** @author cb-prasanna */
 interface Operation {
   int process(int a, int b);
+
+  default void process2(int a, int b) {
+    System.out.println("Test");
+  }
 }
