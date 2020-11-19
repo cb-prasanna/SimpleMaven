28145ba84ce1640df03f5350781369b9d4b1571c
cb-prasanna <49482871+cb-prasanna@users.noreply.github.com>
2020-11-19 13:51:23 +0530

    Revert "Dummy"

--- src/test/java/com/sai/objects/SubOperationTest.java
+++ src/test/java/com/sai/objects/SubOperationTest.java
@@ -1,15 +1,17 @@
 package com.sai.objects;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
-/** @author cb-prasanna */
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author cb-prasanna
+ */
 class SubOperationTest {
-  @Test
-  void processAdd() {
-    Operation op = new SubOperation();
-    int processed = op.process(2, 4);
-    assertEquals(-2, processed);
-  }
+    @Test
+    void processAdd() {
+        Operation op = new SubOperation();
+        int processed = op.process(2, 4);
+        assertEquals(-2, processed);
+    }
 }
